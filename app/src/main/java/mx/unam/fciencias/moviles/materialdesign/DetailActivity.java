1a8af29ffcffd9540975c8f530ec42c0c5643cc3
monica <monmm@ciencias.unam.mx>
2025-05-20 09:50:24 -0600

    Correccion de codigo y funcionamiento

--- app/src/main/java/mx/unam/fciencias/moviles/materialdesign/DetailActivity.java
+++ app/src/main/java/mx/unam/fciencias/moviles/materialdesign/DetailActivity.java
@@ -20,7 +20,7 @@ public class DetailActivity extends BaseActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_detail);
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.color_detail_holder), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
